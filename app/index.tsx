a998bccaf795932775a8d965747f4ae44dbac97e
PezKid <pezmanmc@gmail.com>
2025-07-24 19:46:20 -0700

    Add search bar, add bar, and todo item checkbox and delete icon. Update styles.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,13 @@
 import { Ionicons } from '@expo/vector-icons';
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Checkbox } from 'expo-checkbox';
+import { FlatList, Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+type TodoType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+}
 
 export default function Index() {
 
@@ -8,12 +15,10 @@ export default function Index() {
     { id: 1, title: "Todo 1", isDone: false, },
     { id: 2, title: "Todo 2", isDone: false, },
     { id: 3, title: "Todo 3", isDone: false, },
-    { id: 4, title: "Todo 4", isDone: false, },
+    { id: 4, title: "Todo 4", isDone: true, },
     { id: 5, title: "Todo 5", isDone: false, },
     { id: 6, title: "Todo 6", isDone: false, },
     { id: 7, title: "Todo 7", isDone: false, },
-    { id: 8, title: "Todo 8", isDone: false, },
-    { id: 9, title: "Todo 9", isDone: false, },
   ]
 
   return (
@@ -30,19 +35,42 @@ export default function Index() {
             />
           </TouchableOpacity>
         </View>
+
+        <View style={styles.searchBar}>
+          <Ionicons name='search' size={24} color={'#333'}/>
+          <TextInput placeholder='Search' style={styles.searchInput} clearButtonMode='always'/>
+        </View>
+
         <FlatList
           data={todoData}
           renderItem={({ item }) => (
-            <View>
-              <Text>{item.title}</Text>
-            </View>
+            <TodoItem item={item} />
           )}
           keyExtractor={item => String(item.id)}
         />
+
+        <KeyboardAvoidingView style={styles.footer} behavior='padding' keyboardVerticalOffset={10} >
+          <TextInput placeholder='Add item' style={styles.newTodoInput} />
+          <TouchableOpacity style={styles.addButton} onPress={() => {} }>
+            <Ionicons name='add' size={24} color={'#ffffff'} />
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const TodoItem = ({item}: {item: TodoType}) => (
+  <View style={styles.todoContainer}>
+    <View style={styles.todoInfoContainer}>
+      <Checkbox value={item.isDone} color={item.isDone ? '#aaaaff' : undefined}/>
+      <Text style={[styles.todoText, item.isDone && {textDecorationLine: 'line-through'}]}>{item.title}</Text>
+    </View>
+    <TouchableOpacity onPress={() => {}}>
+      <Ionicons name='trash' size={24} color={'#ffaaaa'}/>
+    </TouchableOpacity>
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -55,5 +83,56 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  searchBar: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    padding: 16,
+    borderRadius: 10,
+    gap: 10,
+    marginBottom: 20,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+  },
+  todoContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    padding: 16,
+    borderRadius: 10,
+    margin: 4,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  todoInfoContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+  },
+  todoText: {
+    fontSize: 16,
+    color: '#333'
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  newTodoInput: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    padding: 16,
+    borderRadius: 10,
+    margin: 10,
+    color: '#333 ',
+    fontSize: 16
+  },
+  addButton: {
+    backgroundColor: '#aaaaff',
+    borderRadius: 10,
+    padding: 8,
+    marginLeft: 20,
   },
 });
